37c912303b0f2d557b6dc68b91a1e47e904175ce
FerShibli <fernandoluiz.1991@gmail.com>
2025-09-17 22:09:27 -0300

    refactor(RegisterScreen): extract styles into a separate object for better readability

--- my-app-4/screens/RegisterScreen.js
+++ my-app-4/screens/RegisterScreen.js
@@ -2,15 +2,32 @@ import React from "react";
 import { Button, Text, TextInput, View } from "react-native";
 
 function RegisterScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', gap: 10 }}>
-            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Register Screen</Text>
-            <TextInput placeholder="Nome" style={{ borderWidth: 1, borderColor: 'gray', width: '80%', padding: 10 }} />
-            <TextInput placeholder="Email" style={{ borderWidth: 1, borderColor: 'gray', width: '80%', padding: 10 }} />
-            <TextInput placeholder="Senha" secureTextEntry style={{ borderWidth: 1, borderColor: 'gray', width: '80%', padding: 10 }} />
-            <Button title="Cadastrar" onPress={() => {}} />
-        </View>
-    )
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 10,
+      }}
+    >
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>Register Screen</Text>
+      <TextInput placeholder="Nome" style={styles.textInput} />
+      <TextInput placeholder="Email" style={styles.textInput} />
+      <TextInput placeholder="Senha" secureTextEntry style={styles.textInput} />
+      <TextInput placeholder="Senha" secureTextEntry style={styles.textInput} />
+      <Button title="Cadastrar" onPress={() => {}} />
+    </View>
+  );
 }
+
+const styles = {
+  textInput: {
+    borderWidth: 1,
+    borderColor: "gray",
+    width: "80%",
+    padding: 10,
+  },
+};
 
 export default RegisterScreen;
