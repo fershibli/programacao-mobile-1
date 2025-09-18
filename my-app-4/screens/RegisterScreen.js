7ff9938d0e5035f1f78710e8761a212c675e6864
FerShibli <fernandoluiz.1991@gmail.com>
2025-09-17 21:42:02 -0300

    feat(RegisterScreen): implement registration form with input fields and button

--- my-app-4/screens/RegisterScreen.js
+++ my-app-4/screens/RegisterScreen.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
 function RegisterScreen() {
-    return null;
+    return (
+        <View>
+            <Text>Register Screen</Text>
+            <TextInput placeholder="Nome" />
+            <TextInput placeholder="Email" />
+            <TextInput placeholder="Senha" secureTextEntry />
+            <Button title="Cadastrar" onPress={() => {}} />
+        </View>
+    )
 }
 
 export default RegisterScreen;
