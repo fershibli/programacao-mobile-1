a1e8867c8b00147f0a1c78f604efcad6b15f91a1
FerShibli <fernandoluiz.1991@gmail.com>
2025-09-24 20:10:24 -0300

    feat: initialize project structure with essential files and dependencies

---
+++ my-app-5/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        {/* Screens will be added here in the next steps */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
