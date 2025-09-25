49d8c2441e79f6e1a4b178e2bf2b9b12d71f3ed5
FerShibli <fernandoluiz.1991@gmail.com>
2025-09-24 21:08:46 -0300

    feat: implement Cesta screen with layout and styling

--- my-app-5/App.js
+++ my-app-5/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { SafeAreaViewBase } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        {/* Screens will be added here in the next steps */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Cesta">
+          <Stack.Screen
+            name="Cesta"
+            component={require("./screens/Cesta").default}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
